a1d6731ba346c01309416315567bfdfc0368402a
vamos016 <isikkuzgun@proton.me>
2026-05-29 20:23:51 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://eizlkmysqecjhlrahdcr.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_Rk-1wfrHGNj23oWYK5qknQ_sXWEcCvG";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
